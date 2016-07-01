fdfbf0737972c25a6f84e519b72d6ac7a2ebb1de
rémi  <remirobert33530@gmail.com>
2016-07-01 17:47:13 +0800

    add realm gradle

--- app/src/main/java/com/tongjiapp/remirobert/devicecollect/deviceMapper/DeviceMapper.java
+++ app/src/main/java/com/tongjiapp/remirobert/devicecollect/deviceMapper/DeviceMapper.java
@@ -4,14 +4,30 @@ import android.content.Context;
 
 import com.baidu.location.BDLocation;
 
+import java.util.concurrent.Callable;
+
 import bolts.Continuation;
 import bolts.Task;
+import bolts.TaskCompletionSource;
 
 /**
  * Created by remirobert on 01/07/16.
  */
 
 public class DeviceMapper {
+
+    private Task<DeviceRecord>taskRecordDevice() {
+        TaskCompletionSource<DeviceRecord> taskCompletionSource = new TaskCompletionSource<>();
+
+        Task.callInBackground(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+
+                return null;
+            }
+        });
+        return taskCompletionSource.getTask();
+    }
 
     public static void getRecord(Context context, final DeviceMapperListener listener) {
         LocalisationManager localisationManager = new LocalisationManager(context);
